96581b08876bfc29681425a91fb9aac151c35a64
saverx <63292169+saverx@users.noreply.github.com>
2020-04-09 19:44:46 +0200

    Update UtenteDAO.java

--- UtenteDAO.java
+++ UtenteDAO.java
@@ -4,15 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-//anche questo preso dal progetto precedente
-
+//HO AGGIUNTO LA CITTA MA MANCA LA DATA
 public class UtenteDAO {
 
     public Utente doRetrieveByUsernamePassword(String username, String password)
         {
             try (Connection con = ConPool.getConnection()) {
 
-                String query = ("SELECT id, username, passwordhash, email, admin FROM utente WHERE username=? AND passwordhash=SHA1(?)");
+                String query = ("SELECT id, username, passwordhash,email,citta,admin FROM utente WHERE username=? AND passwordhash=SHA1(?)");
                 PreparedStatement ps = con.prepareStatement(query);
                 ps.setString(1, username);
                 ps.setString(2, password);
@@ -25,7 +24,8 @@ public class UtenteDAO {
                     u.setUsername(rs.getString(2));
                     u.setPassword(rs.getString(3));
                     u.setEmail(rs.getString(4));
-                    u.setAdmin(rs.getBoolean(5));
+                    u.setCitta(rs.getString(5));
+                    u.setAdmin(rs.getBoolean(6));
                   return u;
                 }
                 con.close();
@@ -40,13 +40,13 @@ public class UtenteDAO {
     public void doSave(Utente utente) {
         try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps = con.prepareStatement(
-                    "INSERT INTO utente (username,passwordhash,email) VALUES(?,?,?)"
+                    "INSERT INTO utente (username,passwordhash,email,citta) VALUES(?,?,?,?)"
                                    // questo codice è utile solo se si vuole restituire il bean customer completo di id
             );                                                            // in questo caso restituiamo  void per cui è inutile
             ps.setString(1, utente.getUsername());
             ps.setString(2, utente.getPasswordhash());
             ps.setString(3, utente.getEmail());
-
+            ps.setString(4,utente.getCitta());
 
 
             if (ps.executeUpdate() != 1) {
