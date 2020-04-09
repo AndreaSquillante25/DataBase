45fc31604cfd7a9837c82dc1ce42ddf841a1bf1c
LuciaGGaeta <63348388+LuciaGGaeta@users.noreply.github.com>
2020-04-10 01:16:08 +0200

    Add files via upload
    Questo è il db aggiornato per non farvi bestemmiare come ho bestemmiato io questa sera.

--- ProgettoTSW.sql
+++ ProgettoTSW.sql
@@ -11,9 +11,9 @@ citta varchar(20) not null
 );
 
 insert INTO Produttore values
-("1","Panificio", "San James", 84134,"Salerno"),
-("2","Fruttivendolo", "Sant'Andreas",84100,"Salerno"),
-("3","Pastificio", "San Paul", 84100,"Salerno");
+("1","FruttaBio", "San James", 84134,"Salerno"),
+("2","VerduraFresca", "Sant'Andreas",84100,"Salerno"),
+("3","Caseificio Gaeta", "San Paul", 84100,"Salerno");
 
 Create table Cassetta(
 id int auto_increment  Primary key,
@@ -25,7 +25,7 @@ id int auto_increment Primary key,
 prezzo double not null
 );
 
-Create table Cliente(
+Create table utente(
 id int auto_increment Key,
 carrello int,
 foreign key (carrello) references Carrello(id) 
@@ -33,11 +33,9 @@ on delete cascade
 on update cascade,
 username varchar(20) not null,
 email varchar(40) not null,
-password varchar(10) not null, 
+passwordhash varchar(100) not null, 
 admin boolean not null default 0,
-ddnascita date not null,
-cap int not null ,
-via varchar(30) not null,
+ddnascita varchar(15)not null,
 citta varchar(20) not null
 );
 
@@ -46,6 +44,10 @@ id int auto_increment Primary key,
  descrizione varchar(200) not null,
  nome varchar(10) not null
 );
+insert into categoria value
+(1,"si mangia","Verdura"),
+(2,"SonoBuoni","Dolci"),
+(3,"succosa","frutta");
 
 Create table Prodotto(
 id int auto_increment Primary Key,
@@ -64,6 +66,14 @@ offerta boolean not null default 0,
 disponibili int not null,
 immagine varchar(100) not null
 );
+
+insert into prodotto value
+(1,2,1,"Pomodoro","bio","2.4",0,1,"./img/pomodoro.jpg"),
+(4,3,2,"Cioccolato","Fondente","3.50",1,1,"./img/cioccolato.jpg"),
+(3,3,2,"Biscotti","Allo yogurt, senza glutine","3.00",1,10,"./img/biscottiAlloyogurtSenzaGlutine.jpg"),
+(5,1,3,"Fragole","ciao","3.50",1,30,"./img/Fragolebio.jpg"),
+(6,1,3,"Avocado","ciao","3",0,20,"./img/Avocado.jpg"),
+(7,2,1,"rucola","ciao","1.23",1,20,"./img/rucola.jpg");
 
 
 
@@ -121,3 +131,6 @@ on update cascade,
 
 primary key (carrello, cassetta)
 );
+
+
+select * from Prodotto;
